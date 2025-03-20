11724ffd32bd7318c3c065021dd2a259889bf8a2
kuldeepparmar <kuldeep07122002@gmail.com>
2025-03-20 17:51:54 +0530

    remove this line of code
    now i want to remove this code from original code because this code is use for checking how to commit on github

--- src/components/core/Dashboard/instructorCourse/Instructor.jsx
+++ src/components/core/Dashboard/instructorCourse/Instructor.jsx
@@ -5,7 +5,7 @@ const Instructor =()=>{
    return(
     <div>
         <p className="text-richblack-50">this is incompleted as yet</p>
-        <p>check this add in github or not</p>
+        
     </div>
    )
  }
